663920a79c8d91bb2e6948e5ddc21bd9a1606b30
islamshireliyev <islamshireliyev99@gmail.com>
2022-02-22 21:20:21 +0400

    commit 1

---
+++ ConsoleApp1.Telefonlar/ConsoleApp1/Phone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp.Phone.System
+
+{
+    public class Phone
+    {
+        
+        
+        public int id;
+        public string Producer;
+        public string Version;
+        public string Screen;
+        public string mainCamera;
+        public string operatingSystem;
+        public int Memory;
+        public string productionYear;
+        public int RandomAccessMemory;
+        public string Network;
+        public string Color;
+        public string Weight;
+
+        static int counter = 0;
+        private static string aStr;
+
+        public Phone()
+        {
+            counter++;
+            id = counter;
+        }
+        public override string ToString()
+        {
+            return
+           $"No: {id}\n" +
+           $"Producer: {Producer}\n" +
+           $"Version: {Version}\n" +
+           $"Screen: {Screen}\n" +
+           $"mainCamera: {mainCamera}\n" +
+           $"operatingSystem: {operatingSystem}\n" +
+           $"Memory: {Memory}\n" +
+           $"productionYear: {productionYear}\n" +
+           $"RandomAccessMemory: {RandomAccessMemory}\n" +
+           $"Network: {Network}\n" +
+           $"Color: {Color}\n" +
+           $"Weight: {Weight}\n";
+        }
+
+
+    }
+}
---
+++ ConsoleApp1.Telefonlar/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ConsoleApp1.Metodsintro;
+using ConsoleApp1;
+using ConsoleApp.Phone.System;
+
+namespace ConsoleApp1.PhoneSystem
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+
+            #region
+            int len;
+        l1:
+            len = Monitor.ReadInteger("Nece telefon olsun: ");
+
+            if (len <= 0)
+            {
+                goto l1;
+            }
+
+            Phone[] Phone = new Phone[len];
+            for (int i = 0; i < Phone.Length; i++)
+            {
+                Phone phone = new Phone();
+                Console.WriteLine($"{i + 1} about in Phone: ");
+                phone.Producer = Monitor.ReadString($"Phone producer: ");
+                phone.Version = Monitor.ReadString($"Phone Version: ");
+                phone.Screen = Monitor.ReadString($"Phone Screen: ");
+                phone.mainCamera = Monitor.ReadString($"Phone mainCamera: ");
+                phone.operatingSystem = Monitor.ReadString($"Phone operatingSystem: ");
+                phone.productionYear = Monitor.ReadString($"Phone productionYear: ");
+                phone.Network = Monitor.ReadString($"Phone Network: ");
+                phone.Color = Monitor.ReadString($"Phone Color: ");
+                phone.Weight = Monitor.ReadString($"Phone Weight: ");
+
+                Phone[i] = phone;
+                Console.WriteLine("--------------Next Phone--------------");
+
+            }
+            Console.WriteLine("########################List Of Phones##########################");
+
+            foreach (var item in Phone)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("**********************");
+            }
+
+
+
+            Console.ReadKey();
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
